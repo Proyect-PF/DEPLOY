a868642052dd9e3b02cc8f0c2ce2a77bf45bf112
Juan Rodriguez <juan>
2023-02-12 19:46:52 +0100

    MODIFIED FOR DEPLOY 1

--- api/src/database.ts
+++ api/src/database.ts
@@ -4,7 +4,7 @@ const path = require('path');
 const Sequelize = require('sequelize');
 const process = require('process');
 const basename = path.basename(__dirname);
-const config =require ("../config/config");
+import config from "../config/config";
 
 
 
@@ -12,13 +12,15 @@ const db:any = {};
 
 let sequelize:any;
 
-sequelize = new Sequelize(config.database, config.username, config.password, config);
+sequelize = new Sequelize(config.db_deploy);
+//sequelize = new Sequelize(config.database, config.username, config.password, config);
 
 sequelize.authenticate()
 .then(()=>console.log('- CONEXION A LA BASE DE DATOS EXITOSA -'))
 .catch((err:any)=> console.error("Unable to connect to the database:", err))
 
 fs
+
   .readdirSync(`${__dirname}/models`)
   .filter((file:string) => {
     return (
@@ -32,10 +34,13 @@ fs
     console.log(file, i)
     //TODO: require(path.join(__dirname, file)) => devuelve un objeto con la propiedad default y es un array de funciones de todos los modelos en la carpeta models    
 
+
       const model = require(path.join(`${__dirname}/models`, file)).default(sequelize, Sequelize.DataTypes);
+
       db[model.name] = model;
     
   });
+  ///../../api/src/models
 
 // Esto es para hacer el associate de los modelos que lo necesiten
 // Opcion 1
